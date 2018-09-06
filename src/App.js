b6bc85fb9158ded44592b0079495a30631e45215
shultztom <shultzthomask@sau.edu>
2018-09-05 20:26:05 -0500

    changed title

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Dinosaur Front End</h1>
+          <h1 className="App-title">Dinosaur Facts!</h1>
         </header>
         <div className="dinosaurInfo">
           <div className="dinosaurName">
