c6ade7ee3a1b6dcc7bfff91d2d1f6157e4edb54f
shultztom <shultzthomask@sau.edu>
2018-09-05 20:14:32 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import logo from "./blue-dinosaur.svg";
+import "./App.css";
+import axios from "axios";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: null,
+      description: null
+    };
+  }
+
+  fetchData = () => {
+    axios
+      .get("https://dinosaur-facts-api.herokuapp.com/dinosaurs/random")
+      .then(response => {
+        if (response.status === 200 && response.statusText === "OK") {
+          this.setState({
+            name: response.data.Name,
+            description: response.data.Description
+          });
+        } else {
+          console.log("Error getting dinosaur :(");
+          this.setState({
+            name: "Error getting dinosaur :(",
+            description: null
+          });
+        }
+      })
+      .catch(error => {
+        console.log("Error getting dinosaur :(");
+        this.setState({ name: "Error getting dinosaur :(", description: null });
+      });
+  };
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  NewDinoButton() {
+    return (
+      <button className="styledButton" onClick={() => this.fetchData()}>
+        New Dinosaur
+      </button>
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Dinosaur Front End</h1>
+        </header>
+        <div className="dinosaurInfo">
+          <div className="dinosaurName">
+            <p>{this.state.name}</p>
+          </div>
+          <div className="dinosaurDescription">
+            <p>{this.state.description}</p>
+          </div>
+          {this.NewDinoButton()}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
