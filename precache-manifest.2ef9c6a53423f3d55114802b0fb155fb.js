4cf9624f0a4ce0dba3f666611c92f7d914260eaa
shultztom <shultzthomask@sau.edu>
2019-05-19 18:09:46 +0000

    Updates

---
+++ precache-manifest.2ef9c6a53423f3d55114802b0fb155fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ecf56bcbf2f47d86959b2372cdba695",
+    "url": "/dinosaur-fact-react/index.html"
+  },
+  {
+    "revision": "41ab52943e691eb03f95",
+    "url": "/dinosaur-fact-react/static/css/main.23685240.chunk.css"
+  },
+  {
+    "revision": "0e995d9bf4391c211264",
+    "url": "/dinosaur-fact-react/static/js/2.b48fc803.chunk.js"
+  },
+  {
+    "revision": "41ab52943e691eb03f95",
+    "url": "/dinosaur-fact-react/static/js/main.54f51a75.chunk.js"
+  },
+  {
+    "revision": "a0a26396535b13a7ec60",
+    "url": "/dinosaur-fact-react/static/js/runtime~main.0d2432f5.js"
+  },
+  {
+    "revision": "f423445b0d9699052c507997781f574b",
+    "url": "/dinosaur-fact-react/static/media/blue-dinosaur.f423445b.svg"
+  }
+]);
